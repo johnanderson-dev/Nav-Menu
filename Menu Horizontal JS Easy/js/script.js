b62310007ec811c3583639de066525be8dd2c6af
johnanderson-dev <johnanderson.developer@gmail.com>
2021-08-27 09:39:11 -0300

    menu horizontal js Add

---
+++ Menu Horizontal JS Easy/js/script.js
@@ -0,0 +1,11 @@
+function acao(){
+    let menu = document.getElementById("menu");
+
+    if(menu.className === "menu"){
+        menu.className += " menu-mobile";
+    }
+
+    else{
+        menu.className = "menu";
+    }
+}
